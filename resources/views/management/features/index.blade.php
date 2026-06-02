afc3cfbd20fa2c6f9475d8554b7e771b1db56d79
taufiqq768 <taufiqq768@gmail.com>
2026-06-02 14:16:29 +0700

    perubahan warna font filter

--- resources/views/management/features/index.blade.php
+++ resources/views/management/features/index.blade.php
@@ -41,7 +41,7 @@
                     <div class="flex-1">
                         <input type="text" name="search" placeholder="Search by slug or name..."
                             value="{{ request('search') }}"
-                            class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500">
+                            class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 text-gray-900">
                     </div>
                     <button type="submit"
                         class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-lg transition">
